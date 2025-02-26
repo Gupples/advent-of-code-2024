f1d9399dbfbd3334b2c73de4cff6be0bc3c424b7
Gupples <gupples224@gmail.com>
2025-02-26 15:01:57 -0700

    Started on Day01

---
+++ day01/day01.py
@@ -0,0 +1,30 @@
+'''
+System Procedure:
+1. Read the file data
+2. Parse the file's data into two lists. 
+3. Sort the two lists.
+4. Compare the difference between each complimentary item in the lists.
+5. Find the sum of the differences.
+6. Print the difference to the screen.
+'''
+
+'''
+READ FILE
+Reads a .txt file to return two lists
+Input:  'filename' - the name of the file to be read.
+Output: 'data' - a list of numeric strings.
+'''
+def read_file(file_name):
+    
+    return
+
+
+'''
+PARSE DATA
+Takes a list of strings and splits the items into two lists.
+Input: 'data' - a list of numeric strings
+Output: 'lists' - a dictionary containing the two lists.
+'''
+def parse_data(data):
+    
+    return
---
+++ day01/testDay01.py
@@ -0,0 +1,30 @@
+'''Driver for Day01.py'''
+from day01 import *
+import pytest
+
+'''Test read_file'''
+# Test against the key
+def test_read_file_key():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+
+'''Test parse_data'''
+# Test an empty string
+def test_parse_data_empty():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+def test_parse_data_uneven():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+# ...
