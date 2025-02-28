3de05d3571a07cb4faf531828581e24913e58806
Gupples <gupples224@gmail.com>
2025-02-27 18:43:14 -0700

    Completed task for Day 01

--- day01/day01.py
+++ day01/day01.py
@@ -11,20 +11,127 @@ System Procedure:
 '''
 READ FILE
 Reads a .txt file to return two lists
-Input:  'filename' - the name of the file to be read.
-Output: 'data' - a list of numeric strings.
+Input:      'filename' - the name of the file to be read.
+Output:     'data' - a list of numeric strings.
 '''
 def read_file(file_name):
-    
-    return
+    # Try to read the file
+    try:
+        with open(file_name, 'r') as file:
+            # Get each line from the file.
+            lines = file.readlines()
+            data = []
+
+            # Remove \n from the list
+            for line in lines:
+                data.append(line.replace('\n', ''))
+            print(data)
+            return data
+    except:
+        print(f"File '{file_name}' could not be read.")
+        return []
 
 
 '''
 PARSE DATA
 Takes a list of strings and splits the items into two lists.
-Input: 'data' - a list of numeric strings
-Output: 'lists' - a dictionary containing the two lists.
+Input:      'data' - a list of numeric strings
+Output:     'lists' - a list containing the two lists.
 '''
 def parse_data(data):
+    lists = [[],[]]
+    for line in data:
+        numbers = line.split()
+        lists[0].append(int(numbers[0]))
+        lists[1].append(int(numbers[1]))
+    return lists
+
+'''
+GET SIMILARITY SCORE
+(added in Part 2)
+Takes two lists and counts how often each number in the first list appears in
+the second list. Similarity score is the sum of the products of each value and
+the number of times it appears in the second list (for example, if the number
+3 appeared 4 times in the list, similarity would increase by 12 (3 * 4).) 
+Input:      'list1' - The first list to be compared
+            'list2' - The second list to be compared
+OUTPUT:     'sum_similarity' - The total of the products of the number and 
+                        their occurances.
+'''
+def get_similarity_score(list1, list2):
+    # If both lists are empty, there's noting to sum.
+    if (len(list1) == len(list2) == 0):
+        return 0
+    sum_similarity = 0
+    value_catalogue = {}
+
+    # Get all the values in the first list
+    for i in range(len(list1)):
+
+        # Add new values in the first list to the catalogue.
+        if (value_catalogue.get(list1[i]) == None):
+            value_catalogue[list1[i]] = 0
+
+    # Find overlaps in the second list.
+    for i in range(len(list2)):
+        if list2[i] in value_catalogue:
+            value_catalogue.update({list2[i]: value_catalogue[list2[i]] + 1})
     
-    return
+    for i in range(len(list1)):
+        sum_similarity += (list1[i] * value_catalogue[list1[i]])
+
+    return sum_similarity
+
+
+'''
+GET TOTAL DIFFERENCE
+Takes two lists and returns the sum difference of each item in the lists.
+Input:      'list1' - The first list to be compared
+            'list2' - The second list to be compared
+Output:     'difference' - The total difference between the two lists.
+'''
+def get_total_difference(list1, list2):
+    # If both lists are empty, return no difference.
+    if (len(list1) == len(list2) == 0):
+        return 0
+    sum_difference = 0
+
+    # Continue for as long as the longest list is.
+    length = (len(list1) if len(list1) > len(list2) else len(list2))
+    for i in range(length):
+
+        # If a list ran out, count it as 0
+        val1 = (list1[i] if list1[i] else 0)
+        val2 = (list2[i] if list2[i] else 0)
+
+        # Get the difference
+        difference = abs(val1 - val2)
+        sum_difference += difference
+    return sum_difference
+
+def main():
+    # Read the file data
+    file_name = input("Enter the file's name: ")
+    raw_data = read_file(file_name)
+
+    # Parse the file data
+    data = parse_data(raw_data)
+    list1 = data[0]
+    list2 = data[1]
+
+    # Sort the data
+    list1.sort()
+    list2.sort()
+
+    # Get similarity rating
+    similarity = get_similarity_score(list1, list2)
+    print(f"Similarity Score: {similarity}.")
+
+    # Compare the lists and find the sum of the differences
+    difference = get_total_difference(list1, list2)
+
+    # Print to the screen
+    print(f"The difference is: {difference}.")
+
+if __name__ == "__main__":
+    main()
--- day01/testDay01.py
+++ day01/testDay01.py
@@ -2,14 +2,68 @@
 from day01 import *
 import pytest
 
+# Create a standard list for testing
+def setup_standard_list():
+    list = [1, 2, 3, 4, 5]
+    return list
+
+# Create a list filled with 0's for testing
+def setup_empty_list():
+    list = [0, 0, 0, 0, 0]
+    return list
+
+
+'''    -------BEGIN TESTING-------    '''
+
 '''Test read_file'''
+# Test against nonexistant file
+def test_read_file_nonexistant():
+    # SETUP
+    file_name = "not_here.txt"
+    result = setup_standard_list()
+
+    # EXERCISE
+    result = read_file(file_name)
+
+    # VERIFY
+    assert file_name == "not_here.txt"
+    assert result==[]
+
+    # TEARDOWN
+
+# Test against empty file
+def test_read_file_empty():
+    # SETUP
+    file_name = "day01/test_files/empty.txt"
+    result = setup_standard_list()
+
+    # EXERCISE
+    result = read_file(file_name)
+
+    # VERIFY
+    assert file_name == "day01/test_files/empty.txt"
+    assert result==[]
+
+    # TEARDOWN
+
 # Test against the key
 def test_read_file_key():
     # SETUP
+    file_name = "day01/01Key.txt"
+    result = setup_empty_list()
+
     # EXERCISE
+    result = read_file(file_name)
+
     # VERIFY
+    assert file_name == "day01/01Key.txt"
+    assert result == ['3   4', '4   3', '2   5', '1   3', '3   9', '3   3']
+
     # TEARDOWN
-    return
+
+# Test against uneven file (left longer)
+
+# Test against uneven file (right longer)
 
 
 '''Test parse_data'''
@@ -21,10 +75,100 @@ def test_parse_data_empty():
     # TEARDOWN
     return
 
+# Test one line
+def test_parse_data_one_line():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# 
+# 
+# 
+# 
+#  
+
 def test_parse_data_uneven():
     # SETUP
     # EXERCISE
     # VERIFY
     # TEARDOWN
     return
-# ...
+#
+# 
+# 
+# 
+#  ...
+
+'''Test get_similarity_score'''
+# Test empty strings
+# 
+#  ...
+
+'''Test get_total_difference'''
+# Test empty strings
+def test_get_total_difference_empty():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# Test left string longer
+def test_get_total_difference_left_longer():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+# Test right string longer
+def test_get_total_difference_right_longer():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# Test one element same value 
+def test_get_total_difference_one_value():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# Test one element left larger value by a little
+def test_get_total_difference_short_left_little_difference():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# Test one element left larger value by a lot
+def test_get_total_difference_short_left_large_difference():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# Test one element right larger value by a little
+def test_get_total_difference_short_right_little_difference():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+
+# Test one element right larger value by a lot
+def test_get_total_difference_short_right_large_difference():
+    # SETUP
+    # EXERCISE
+    # VERIFY
+    # TEARDOWN
+    return
+#  
+
+pytest.main(["-v", "--tb=line", "-rN", __file__])
